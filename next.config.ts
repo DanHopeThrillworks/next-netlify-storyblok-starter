956df5fb6a776fcd2feae75e2ebfc7e902629d70
Dan Hope <dhope@DanHope-Mac.local>
2025-09-19 10:41:12 -0400

    standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   // Do not fail production builds on ESLint errors
   eslint: {
     ignoreDuringBuilds: true,
